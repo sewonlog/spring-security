23039fb1b4c1faeb73b71c96a6c051479476aabb
sewon <sewonlog@gmail.com>
2024-03-20 19:34:02 +0900

    2024.03.20
    -기본 구성

--- src/main/java/com/example/securitypractice/config/RedisConfig.java
+++ src/main/java/com/example/securitypractice/config/RedisConfig.java
@@ -1,7 +1,14 @@
 package com.example.securitypractice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
 }
